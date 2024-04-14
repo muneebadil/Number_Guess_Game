80a45146a384fa3cbe51edb9e93704014715e4e8
muneebadil <mohammadmuneeb197@gmail.com>
2024-04-14 18:29:13 +0500

    Number_Guess_Game_Muneeb

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\t Welcome to Muneeb Guess Game \n");
+const randnum = Math.floor(Math.random() * 3 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessnum",
+        type: "number",
+        message: "Enter Guess Number (1-3):",
+    },
+]);
+if (answer.userguessnum === randnum) {
+    console.log("\n\t congratulation you guess correct number");
+}
+else {
+    console.log("\n\t sorry you gues wrong");
+}
